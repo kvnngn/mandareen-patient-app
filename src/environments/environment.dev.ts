6c8200a66a6cad6aa3c3080c8835ebc841245017
NGUYEN Kévin <kevin3.nguyen@epitech.eu>
2019-01-16 11:53:27 +0100

    env for master

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     isDebugMode: false,
-    origin: 'https://192.168.0.47:1234',
+    origin: 'https://163.5.84.112:1234',
     version: '1.0.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     isDebugMode: false,
-    origin: 'https://192.168.0.47:1234',
+    origin: 'https://163.5.84.112:1234',
     //origin: 'https://localhost:1234',
     version: '1.0.0'
 };
